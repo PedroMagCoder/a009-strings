ca65548718c930306c890d7821c90431d55ad2ef
PedroMagCoder <pedromagno999@gmail.com>
2022-07-26 18:10:48 -0300

    exercício aula 009

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,10 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaStringSemEspaço = minhaString.trim()
+
+console.log(minhaString)
+
+console.log(minhaStringSemEspaço)
+
+console.log(`A frase inicial tem ${minhaString.length} caracteres\nA frase alterada tem ${minhaStringSemEspaço.length} caracteres`)
+
+console.log(minhaStringSemEspaço.replace("________","sticioso"))
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,16 @@
 //Crie a const para a frase aqui
+
+const string = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"`
+
+const frase = string
+
+console.log(frase)
+
+const frase2 = (frase.replace("verde","rosa"))
+const frase3 = (frase2.replace("azul","laranja"))
+
+console.log(frase3)
+
+const frase4 = (frase3.replace("mas não deixe o gato sair","MAS NÃO DEIXE O GATO SAIR"))
+ 
+console.log(frase4)
